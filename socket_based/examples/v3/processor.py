2f90cc6b6030681447b1ae25407abe547e87d3fa
Nick Shu <nicolas.s.shu@gmail.com>
2021-03-29 19:27:30 -0400

    Modified example script to take LogMelSpectrogram

--- socket_based/examples/v3/processor.py
+++ socket_based/examples/v3/processor.py
@@ -1,4 +1,15 @@
 from audiosockets import BaseProcessorSocket
+from audiosockets.utils import LogMelSpectrogram
 
-processor = BaseProcessorSocket("VAD", "server_info.json")
+class LogMelSpecProcessor(BaseProcessorSocket):
+    def __init__(self,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def process_data(self,data):
+        fs = data["fs"]
+        audio = data["data"]
+        lms = LogMelSpectrogram(fs)(audio)
+        print(lms.shape)
+
+processor = LogMelSpecProcessor("VAD", "server_info.json")
 processor.start()
