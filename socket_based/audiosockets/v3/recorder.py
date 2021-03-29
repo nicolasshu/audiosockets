9dd2ab8b04b983817bd4835247486738da157296
Nick Shu <nicolas.s.shu@gmail.com>
2021-03-29 19:02:30 -0400

    Made recorder into a class that can be easily imported

--- socket_based/audiosockets/v3/recorder.py
+++ socket_based/audiosockets/v3/recorder.py
@@ -23,6 +23,29 @@ class RecordSocket(ClientSocket):
         try:
             # Try to connect to the server
             self.connect(self.SERVER, self.PORT)
+
+            # Prepare Audio Recording
+            parser = argparse.ArgumentParser()
+            parser.add_argument("-l","--list_devices", action="store_true",help="Show list of all devices")
+            parser.add_argument('-d', '--device', type=int, default = 7, help='input device (numeric ID or substring)')
+            parser.add_argument('-r', '--samplerate', type=int, help='sampling rate')
+            parser.add_argument('-c', '--channels', type=int, default=1, help='number of input channels')
+            parser.add_argument('-T', '--samplingperiod', type=float, default=0.5, help='sampling period in seconds')
+            self.args = parser.parse_args()
+
+            # If a sample rate is not established, obtain the 
+            # sampling rate from the chosen recording device
+            if self.args.samplerate is None:
+                device_info = sd.query_devices(self.args.device, 'input')
+                self.args.samplerate = device_info['default_samplerate']
+
+            # Define the sampling perior of sending the data
+            self.interval = self.args.samplingperiod # seconds
+            self.interval *= 1000
+
+            # Create a queue object
+            self.q = queue.Queue()
+
         except ConnectionRefusedError:
             # If you can't connect, keep trying to connect once a second
             logging.info("Trying to connect")
@@ -40,70 +63,51 @@ class RecordSocket(ClientSocket):
         # Send the object
         self.send_obj(self.client, audio_data)
 
-def queue_cb(data, frames, times, status):
-    # Put the current stream of data into a queue
-    q.put(data.copy())
+    def queue_cb(self,data, frames, times, status):
+        # Put the current stream of data into a queue
+        self.q.put(data.copy())
+
+    def start(self):
+        # Initialize a stream of audio
+        with sd.InputStream(samplerate=self.args.samplerate, device = self.args.device, channels = self.args.channels, callback=self.queue_cb):
+            print('#' * 34+'\npress Ctrl+C to stop the recording\n'+'#' * 34)
+            start_time = datetime.datetime.now()
+
+            # Initialize an accumulated list of data
+            data = []
+            while True:
+                try:
+                    # Compute the time delta
+                    dt = datetime.datetime.now() - start_time
+
+                    # Add the chunk of data collected so far to a list
+                    data.append(self.q.get())
+
+                    # For every {samplingperiod},
+                    if dt.microseconds/1000 >= self.interval:
+                        # Create a new start time
+                        start_time = datetime.datetime.now()
+
+                        # Compile all of the data and 
+                        # reset the accumulated list of data
+                        collected_audio = np.concatenate(data,axis=0)
+                        data = []
+
+                        # Send the audio data to the server
+                        self.send_audio_data(collected_audio,self.args.samplerate)
+                        logging.info(f"Send audio data")
+                except KeyboardInterrupt:
+                    # If you choose to kill the process, send a disconnecting 
+                    # message to the server and break out
+                    self.send_text(self.client,self.DISCONNECT_MSG)
+                    logging.info("\nDisconnected from server")
+                    break
+                except Exception as e:
+                    print(e)
 
 if __name__ == "__main__":
-    # Prepare Audio Recording
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-l","--list_devices", action="store_true",help="Show list of all devices")
-    parser.add_argument('-d', '--device', type=int, default = 7, help='input device (numeric ID or substring)')
-    parser.add_argument('-r', '--samplerate', type=int, help='sampling rate')
-    parser.add_argument('-c', '--channels', type=int, default=1, help='number of input channels')
-    parser.add_argument('-T', '--samplingperiod', type=float, default=0.5, help='sampling period in seconds')
-    args = parser.parse_args()
-
-    # If a sample rate is not established, obtain the 
-    # sampling rate from the chosen recording device
-    if args.samplerate is None:
-        device_info = sd.query_devices(args.device, 'input')
-        args.samplerate = device_info['default_samplerate']
 
     # Obtain the Network Information
     info = "server_info.json"
     recorder = RecordSocket(info)
-
-    # Define the sampling perior of sending the data
-    interval = args.samplingperiod # seconds
-    interval *= 1000
-
-    # Create a queue object
-    q = queue.Queue()
-
-    # Initialize a stream of audio
-    with sd.InputStream(samplerate=args.samplerate, device = args.device, channels = args.channels, callback=queue_cb):
-        print('#' * 34+'\npress Ctrl+C to stop the recording\n'+'#' * 34)
-        start_time = datetime.datetime.now()
-
-        # Initialize an accumulated list of data
-        data = []
-        while True:
-            try:
-                # Compute the time delta
-                dt = datetime.datetime.now() - start_time
-
-                # Add the chunk of data collected so far to a list
-                data.append(q.get())
-
-                # For every {samplingperiod},
-                if dt.microseconds/1000 >= interval:
-                    # Create a new start time
-                    start_time = datetime.datetime.now()
-
-                    # Compile all of the data and 
-                    # reset the accumulated list of data
-                    collected_audio = np.concatenate(data,axis=0)
-                    data = []
-
-                    # Send the audio data to the server
-                    recorder.send_audio_data(collected_audio,args.samplerate)
-                    logging.info(f"Send audio data")
-            except KeyboardInterrupt:
-                # If you choose to kill the process, send a disconnecting 
-                # message to the server and break out
-                recorder.send_text(recorder.client,recorder.DISCONNECT_MSG)
-                logging.info("\nDisconnected from server")
-                break
-            except Exception as e:
-                print(e)
+    recorder.start()
