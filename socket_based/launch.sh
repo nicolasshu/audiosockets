72b7bfceb82c302cbfcd762b0f8ddb25cf8153a8
Nick Shu <nicolas.s.shu@gmail.com>
2021-03-29 18:41:26 -0400

    Updated a launch script

--- socket_based/launch.sh
+++ socket_based/launch.sh
@@ -4,9 +4,12 @@ echo ${port}
 lsof -t -i:${port} | xargs kill -9
 sleep 3
 tmux new-session -d
-tmux send 'python my_server.py' ENTER
+tmux send 'python server.py' ENTER
 sleep 1
 tmux split-window -v
 tmux send 'python recorder.py' ENTER
+sleep 1
+tmux split-window -v
+tmux send 'python baseprocessor.py' ENTER
 sleep 1
 tmux attach-session
