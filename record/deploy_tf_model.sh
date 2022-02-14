ed3054d9df85fa073ff0f06df44ede2a834719e4
Nicolas Shu <nicolas.s.shu@gmail.com>
2022-02-14 17:11:21 -0500

    Removed the recording scripts

--- record/deploy_tf_model.sh
+++
@@ -1,2 +0,0 @@
-here="/home/nickshu/Documents/experiments/voice_activity/saved_models"
-docker run -p 8501:8501 --name tfresnet --mount type=bind,source=$here/super-bird-20,target=/models/super-bird-20 -e MODEL_NAME=super-bird-20 -t tensorflow/serving &
